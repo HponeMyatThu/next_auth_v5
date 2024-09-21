1eba462078ea2170e24b2b1e1c4adce1395d47dc
Hpone Myat Thu <hponemy8tthu@gmail.com>
2024-09-22 00:35:20 +0630

    add ssl to project

---
+++ components/auth/social.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from "react"
+
+const Social = () => {
+	return (
+		<div
+			className="
+    flex items-center w-full gap-x-2
+    "
+		></div>
+	)
+}
+
+export default Social
